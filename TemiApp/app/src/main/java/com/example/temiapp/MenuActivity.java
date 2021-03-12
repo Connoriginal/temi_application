701affc0037520a6a0f12a278e0fe292f6544885
1004gite <1004gite@naver.com>
2021-03-12 17:00:41 +0900

    어댑터 리스트뷰 연결 및 버튼 리스너 생성

--- TemiApp/app/src/main/java/com/example/temiapp/MenuActivity.java
+++ TemiApp/app/src/main/java/com/example/temiapp/MenuActivity.java
@@ -1,15 +1,21 @@
 package com.example.temiapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.temiapp.temi.RoboTemiListener;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -18,6 +24,11 @@ import java.util.ArrayList;
 public class MenuActivity extends AppCompatActivity{
 
     RoboTemiListener listener;
+    ListView menuList;
+    MyAdapter menuAdapter;
+    ListView selectList;
+    MyAdapter selectAdapter;
+    Button btDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,30 @@ public class MenuActivity extends AppCompatActivity{
         setContentView(R.layout.activity_menu);
         listener = new RoboTemiListener();
         listener.init();
+
+        menuList = (ListView) findViewById(R.id.listviewForMenu);
+        menuAdapter = new MyAdapter(true);
+
+        menuList.setAdapter(menuAdapter);
+
+        selectList = (ListView) findViewById(R.id.listviewForSelect);
+        selectAdapter = new MyAdapter(false);
+        selectList.setAdapter(selectAdapter);
+
+        btDone = (Button) findViewById(R.id.btDone);
+        btDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int totalPrice=0;
+                for(MenuData menuData: selectAdapter.getItems()){
+                    totalPrice += menuData.price;
+                }
+                Toast.makeText(getApplicationContext(),"총 "+Integer.toString(totalPrice)+"원 입니다.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        for(int i =1;i<10;i++)
+            menuAdapter.add(new MenuData("test"+Integer.toString(i),1));
 
     }
 
@@ -38,18 +73,14 @@ public class MenuActivity extends AppCompatActivity{
 
 
 
-
-
-
-
-
-
-    public class MenuAdapter extends BaseAdapter {
+    public class MyAdapter extends BaseAdapter {
 
         public ArrayList<MenuData> items;
+        public boolean is_menu = false;
 
-        public MenuAdapter() {
+        public MyAdapter(boolean is_menu) {
             this.items = new ArrayList<>();
+            this.is_menu = is_menu;
         }
 
 
@@ -58,6 +89,9 @@ public class MenuActivity extends AppCompatActivity{
             return items.size();
         }
 
+        public ArrayList<MenuData> getItems(){
+            return items;
+        }
         @Override
         public MenuData getItem(int i) {
             return items.get(i);
@@ -90,10 +124,41 @@ public class MenuActivity extends AppCompatActivity{
 
             MenuData menuData = getItem(i);
             if(menuData != null) {
-                TextView nameText = (TextView)findViewById(R.id.nameText);
-                TextView priceText = (TextView)findViewById(R.id.priceText);
+                TextView nameText = (TextView) view.findViewById(R.id.nameText);
+                TextView priceText = (TextView) view.findViewById(R.id.priceText);
                 nameText.setText(menuData.menuname);
                 priceText.setText(Integer.toString(menuData.price));
+
+                LinearLayout l = (LinearLayout) view.findViewById(R.id.linearForListParent);
+                l.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(is_menu){
+                            selectAdapter.add(menuAdapter.getItem(i));
+                            notifyDataSetChanged();
+                            Toast.makeText(getApplicationContext(),"주문 목록에 추가되었습니다.",Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                            builder.setTitle("주문을 취소하시겠습니까?");
+                            builder.setMessage("\"예\"를 누르면 주문이 취소됩니다.");
+                            builder.setPositiveButton("예",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            selectAdapter.removeItem(i);
+                                            notifyDataSetChanged();
+                                            Toast.makeText(getApplicationContext(),"주문 취소.",Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                            builder.setNegativeButton("아니오",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                        }
+                                    });
+                            builder.show();
+                        }
+                    }
+                });
             }
 
             return view;
