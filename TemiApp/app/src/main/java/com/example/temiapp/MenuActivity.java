da39dfd394788f1fdf37bb82988e89f6f885e662
1004gite <1004gite@naver.com>
2021-03-12 14:47:52 +0900

    리스트뷰 어댑터 구현

--- TemiApp/app/src/main/java/com/example/temiapp/MenuActivity.java
+++ TemiApp/app/src/main/java/com/example/temiapp/MenuActivity.java
@@ -1,10 +1,18 @@
 package com.example.temiapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.temiapp.temi.RoboTemiListener;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 
 public class MenuActivity extends AppCompatActivity{
@@ -27,6 +35,70 @@ public class MenuActivity extends AppCompatActivity{
 
     }
 
+
+
+
+
+
+
+
+
+
+    public class MenuAdapter extends BaseAdapter {
+
+        public ArrayList<MenuData> items;
+
+        public MenuAdapter() {
+            this.items = new ArrayList<>();
+        }
+
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public MenuData getItem(int i) {
+            return items.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        public void add(MenuData item) {
+            items.add(item);
+            notifyDataSetChanged();
+        }
+
+        public void setItem(MenuData menuData,int index){
+            items.set(index,menuData);
+        }
+
+        public void removeItem(int index){
+            items.remove(index);
+        }
+
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if(view == null) {
+                view = getLayoutInflater().inflate(R.layout.menulist, viewGroup, false);
+            }
+
+            MenuData menuData = getItem(i);
+            if(menuData != null) {
+                TextView nameText = (TextView)findViewById(R.id.nameText);
+                TextView priceText = (TextView)findViewById(R.id.priceText);
+                nameText.setText(menuData.menuname);
+                priceText.setText(Integer.toString(menuData.price));
+            }
+
+            return view;
+        }
+    }
 
 
 
